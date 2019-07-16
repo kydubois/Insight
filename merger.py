e08507f1a5ce03e86379b198e2117a58b8cf7db5
kydubois <kydubois28@gmail.com>
2019-07-16 09:43:23 -0700

    Update merger.py

--- merger.py
+++ merger.py
@@ -4,11 +4,12 @@ import pandas as pd
 tagname = input('input tag name: ' )
 
 
-df1 = pd.read_csv(tagname + '_OrganizationsList_with_IDs.csv')
+df1 = pd.read_csv(tagname + '_OrganizationsList_with_Emails.csv')
 df2 = pd.read_csv(tagname + '_ContactEmailList.csv')
 
-with open(tagname + '_Filtered.csv', 'w') as output:
+with open(tagname + '_Filtered_with_Emails.csv', 'w', encoding="utf-8") as output:
     pd.merge(df1, df2, on='Hospital ID').to_csv(output, index=False)
 
-print('======Merger.py complete======')
-print('Make sure to check for and remove any duplicate records')
+print('Merger.py complete')
+print('==== Make sure to check for and remove any duplicate records'=====)
+print("==== Sepeate email column.  May contain more than one email in the column ====")
